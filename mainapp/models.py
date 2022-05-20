913c89732f3748b38b4b441289b363f18d994ece
flex <geroi_cool@mail.ru>
2022-05-20 20:16:01 +0300

    +

--- mainapp/models.py
+++ mainapp/models.py
@@ -9,6 +9,9 @@ class Item(models.Model):
     def __str__(self):
         return self.name
 
+    def get_display_price(self):
+        return self.price/100
+
     class Meta:
         verbose_name = 'объект'
         verbose_name_plural = 'объекты'
